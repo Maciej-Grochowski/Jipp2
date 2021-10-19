d28039b98627f904b75bd03e644202abdc80e6e0
Maciej Grochowski <91936447+mghej4@users.noreply.github.com>
2021-10-19 18:03:25 +0200

    Add files via upload

---
+++ konwersja_tekstu/cmake-build-debug/CMakeFiles/konwersja_tekstu.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/konwersja_tekstu.dir/main.cpp.obj"
+  "konwersja_tekstu.exe"
+  "konwersja_tekstu.exe.manifest"
+  "konwersja_tekstu.lib"
+  "konwersja_tekstu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/konwersja_tekstu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
