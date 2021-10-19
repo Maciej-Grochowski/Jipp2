66160adec0b2582ae28c423e8ea362d7611afbaa
Maciej Grochowski <91936447+mghej4@users.noreply.github.com>
2021-10-19 15:29:45 +0200

    Add files via upload

---
+++ srednia_z_dwoch_liczb/cmake-build-debug/CMakeFiles/srednia_z_dwoch_liczb.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/srednia_z_dwoch_liczb.dir/main.cpp.obj"
+  "srednia_z_dwoch_liczb.exe"
+  "srednia_z_dwoch_liczb.exe.manifest"
+  "srednia_z_dwoch_liczb.lib"
+  "srednia_z_dwoch_liczb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/srednia_z_dwoch_liczb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
