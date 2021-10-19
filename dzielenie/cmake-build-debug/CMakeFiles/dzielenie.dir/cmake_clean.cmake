c918dbcbcd148612435c16bd7a227a89e63a3cf3
Maciej Grochowski <91936447+mghej4@users.noreply.github.com>
2021-10-19 18:20:52 +0200

    Add files via upload

---
+++ dzielenie/cmake-build-debug/CMakeFiles/dzielenie.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dzielenie.dir/main.cpp.obj"
+  "dzielenie.exe"
+  "dzielenie.exe.manifest"
+  "dzielenie.lib"
+  "dzielenie.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dzielenie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
