f2f16f75155ebc24254e44105d46690e9b5c04bd
Maciej Grochowski <91936447+mghej4@users.noreply.github.com>
2021-10-19 15:50:13 +0200

    Add files via upload

---
+++ wieksze_od_zera/cmake-build-debug/CMakeFiles/wieksze_od_zera.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wieksze_od_zera.dir/main.cpp.obj"
+  "wieksze_od_zera.exe"
+  "wieksze_od_zera.exe.manifest"
+  "wieksze_od_zera.lib"
+  "wieksze_od_zera.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wieksze_od_zera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
