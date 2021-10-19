2fc574af3ec58cd77a562c928489d5a5fd2de30c
Maciej Grochowski <91936447+mghej4@users.noreply.github.com>
2021-10-19 17:18:03 +0200

    Add files via upload

---
+++ int_do_wskaznika/cmake-build-debug/CMakeFiles/int_do_wskaznika.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/int_do_wskaznika.dir/main.cpp.obj"
+  "int_do_wskaznika.exe"
+  "int_do_wskaznika.exe.manifest"
+  "int_do_wskaznika.lib"
+  "int_do_wskaznika.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/int_do_wskaznika.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
