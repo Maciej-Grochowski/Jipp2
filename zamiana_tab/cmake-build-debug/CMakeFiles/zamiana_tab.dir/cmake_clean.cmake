1ec6576c6fcf347d6fcdf0fdebd99a21cc905991
Maciej Grochowski <91936447+mghej4@users.noreply.github.com>
2021-10-19 14:29:07 +0200

    Add files via upload

---
+++ zamiana_tab/cmake-build-debug/CMakeFiles/zamiana_tab.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zamiana_tab.dir/main.cpp.obj"
+  "zamiana_tab.exe"
+  "zamiana_tab.exe.manifest"
+  "zamiana_tab.lib"
+  "zamiana_tab.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zamiana_tab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
