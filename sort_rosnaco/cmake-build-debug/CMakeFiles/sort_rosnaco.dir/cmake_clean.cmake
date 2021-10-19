16c465b6ca61e6805bbfff9269daec01286e174c
Maciej Grochowski <91936447+mghej4@users.noreply.github.com>
2021-10-19 16:31:15 +0200

    Add files via upload

---
+++ sort_rosnaco/cmake-build-debug/CMakeFiles/sort_rosnaco.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sort_rosnaco.dir/main.cpp.obj"
+  "sort_rosnaco.exe"
+  "sort_rosnaco.exe.manifest"
+  "sort_rosnaco.lib"
+  "sort_rosnaco.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sort_rosnaco.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
