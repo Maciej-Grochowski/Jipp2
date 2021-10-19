9d9b761c4356648848e2b4c528ae18cd07df70c6
Maciej Grochowski <91936447+mghej4@users.noreply.github.com>
2021-10-19 14:05:32 +0200

    Add files via upload

---
+++ los_zakres/cmake-build-debug/CMakeFiles/los_zakres.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/los_zakres.dir/main.cpp.obj"
+  "los_zakres.exe"
+  "los_zakres.exe.manifest"
+  "los_zakres.lib"
+  "los_zakres.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/los_zakres.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
