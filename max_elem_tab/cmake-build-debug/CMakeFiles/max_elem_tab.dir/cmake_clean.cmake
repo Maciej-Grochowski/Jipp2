9dcc042306e4ee7717a7ee5aeebdd65159946473
Maciej Grochowski <91936447+mghej4@users.noreply.github.com>
2021-10-19 15:01:26 +0200

    Add files via upload

---
+++ max_elem_tab/cmake-build-debug/CMakeFiles/max_elem_tab.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/max_elem_tab.dir/main.cpp.obj"
+  "max_elem_tab.exe"
+  "max_elem_tab.exe.manifest"
+  "max_elem_tab.lib"
+  "max_elem_tab.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/max_elem_tab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
