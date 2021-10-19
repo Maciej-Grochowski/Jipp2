91243fee5e0c4f337a011d8ebac298e7ad82d4b5
Maciej Grochowski <91936447+mghej4@users.noreply.github.com>
2021-10-19 13:53:49 +0200

    Add files via upload

---
+++ pole/cmake-build-debug/CMakeFiles/pole.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pole.dir/main.cpp.obj"
+  "pole.exe"
+  "pole.exe.manifest"
+  "pole.lib"
+  "pole.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pole.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
