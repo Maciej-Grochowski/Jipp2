09cfa314f5501443c60f7ec297ad48868b037f87
Maciej Grochowski <91936447+mghej4@users.noreply.github.com>
2021-10-27 18:34:24 +0200

    Add files via upload

---
+++ lab4-zad11/cmake-build-debug/CMakeFiles/lab4_zad11.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab4_zad11.dir/main.cpp.obj"
+  "lab4_zad11.exe"
+  "lab4_zad11.exe.manifest"
+  "lab4_zad11.lib"
+  "lab4_zad11.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab4_zad11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
