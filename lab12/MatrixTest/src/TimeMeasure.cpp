da59b0beedec10f90956ab2a92af8a405173aa13
Maciej Grochowski <91936447+mghej4@users.noreply.github.com>
2022-01-24 17:50:18 +0100

    Add files via upload

---
+++ lab12/MatrixTest/src/TimeMeasure.cpp
@@ -0,0 +1,22 @@
+//#include "TimeMeasure.h"
+#include "../include/TimeMeasure.h"
+
+void TimeMeasure::timerStart() {
+    start = chrono::high_resolution_clock::now();
+}
+
+void TimeMeasure::timerStop() {
+    end = chrono::high_resolution_clock::now();
+}
+
+long long int TimeMeasure::getTimerTimeInSeconds() {
+    return chrono::duration_cast<chrono::seconds>(end - start).count();
+}
+
+long long int TimeMeasure::getTimerTimeInMilliseconds() {
+    return chrono::duration_cast<chrono::milliseconds>(end - start).count();
+}
+
+long long int TimeMeasure::getTimerTimeInNanoseconds() {
+    return chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+}
